eb6e191752316edbd50b9b8c40d48b892e3b10f8
JAPJEET SINGH <95476734+JAPJEET01@users.noreply.github.com>
2023-12-28 20:36:01 +0530

    Update VoiceSenderReceiver.java

--- VoiceSenderReceiver.java
+++ VoiceSenderReceiver.java
@@ -2,10 +2,13 @@ import javax.sound.sampled.*;
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class VoiceSenderReceiver {
-    public static final int PORT = 8888; // Choose a port number
+    public static final int DEFAULT_PORT = 12345; // Default port
+    public static final int[] DESTINATION_PORTS = { 12346, 12347 }; // Different ports for different destinations
 
     public static void main(String[] args) {
         Thread senderThread = new Thread(() -> startSender());
@@ -25,8 +28,9 @@ public class VoiceSenderReceiver {
 
             // List of destination IP addresses
             List<String> destinationIPs = new ArrayList<>();
-            destinationIPs.add("192.168.29.158"); // Add multiple IP addresses here
-	    destinationIPs.add("192.168.29.157");
+            destinationIPs.add("192.168.29.128"); // Add multiple IP addresses here
+            destinationIPs.add("192.168.29.157");
+
             try (DatagramSocket socket = new DatagramSocket()) {
                 System.out.println("VoiceSender: Ready to send voice...");
 
@@ -35,8 +39,10 @@ public class VoiceSenderReceiver {
 
                 while (true) {
                     int bytesRead = microphone.read(buffer, 0, buffer.length);
-                    for (String ip : destinationIPs) {
-                        packet = new DatagramPacket(buffer, bytesRead, InetAddress.getByName(ip), PORT);
+                    for (int i = 0; i < destinationIPs.size(); i++) {
+                        String ip = destinationIPs.get(i);
+                        int port = DESTINATION_PORTS[i]; // Get the corresponding port for the IP
+                        packet = new DatagramPacket(buffer, bytesRead, InetAddress.getByName(ip), port);
                         socket.send(packet);
                     }
                 }
@@ -54,7 +60,7 @@ public class VoiceSenderReceiver {
             speaker.open(format);
             speaker.start();
 
-            try (DatagramSocket socket = new DatagramSocket(PORT)) {
+            try (DatagramSocket socket = new DatagramSocket(DEFAULT_PORT)) {
                 System.out.println("VoiceReceiver: Ready to receive voice...");
 
                 byte[] buffer = new byte[1024];
